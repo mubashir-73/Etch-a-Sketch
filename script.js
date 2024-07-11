069281adb7c0c29f0942a6e20342ce8feb1c0ee1
mubashir-73 <mubashirsheriff7@gmail.com>
2024-07-11 19:15:44 +0530

    Add input box and function to take user input.

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-const grd = document.querySelector("#Grid");
-let size = 16;
-for (let i = 0; i <= size; i++) {
-  const columndiv = document.createElement("div");
-  columndiv.classList.add("vdivs");
-  for (let j = 0; j <= size; j++) {
-    const subdiv = document.createElement("div");
-    let side = calculateDimensions(size);
-    subdiv.style.width = side;
-    subdiv.style.height = side;
-    subdiv.style.borderColor = "black";
-    subdiv.classList.add("hdivs");
-    columndiv.appendChild(subdiv);
+let size;
+function generateGrid(size = 16) {
+  const grd = document.querySelector("#Grid");
+  for (let i = 0; i <= size; i++) {
+    const columndiv = document.createElement("div");
+    columndiv.classList.add("vdivs");
+    for (let j = 0; j <= size; j++) {
+      const subdiv = document.createElement("div");
+      let side = calculateDimensions(size);
+      subdiv.style.width = side;
+      subdiv.style.height = side;
+      subdiv.style.borderColor = "black";
+      subdiv.classList.add("hdivs");
+      columndiv.appendChild(subdiv);
+    }
+    grd.appendChild(columndiv);
   }
-  grd.appendChild(columndiv);
+  activateTiles();
 }
 function calculateDimensions(size) {
   return Math.sqrt((900 * 900) / (size * size));
 }
 
-let block = document.querySelectorAll(".hdivs");
+function activateTiles() {
+  let block = document.querySelectorAll(".hdivs");
 
-block.forEach((element) => {
-  element.addEventListener("mouseover", () => {
-    element.style.backgroundColor = "black";
+  block.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+      element.style.backgroundColor = "black";
+    });
   });
-});
+}
 
-/*TODO: Add Function to take size of grid from user.*/
+function destroyGrid() {
+  const grd = document.querySelector("#Grid");
+}
+generateGrid();
+const btn = document.querySelector("#buttons");
+btn.addEventListener("click", () => {
+  size = document.getElementById("gridsize").value;
+  destroyGrid();
+  generateGrid(size);
+});
+/*TODO: Add a function that destroys grid before creating next grid.*/
